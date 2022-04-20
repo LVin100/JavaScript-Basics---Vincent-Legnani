22064e7906c89f8bb0a2968f3d044f9bf76f2564
LVin100 <vincent.legnani@gmail.com>
2022-04-20 09:27:17 +0200

    button creati nel js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,32 @@
 let counter=0;
-const inc = document.getElementById('inc');
-const dec = document.getElementById('dec');
-const reset = document.getElementById('reset');
+
+const label = document.createElement("div");
+document.body.appendChild(label)
+label.setAttribute("id", "counter-label");
+let t = document.createTextNode("0");
+label.appendChild(t);
+
+const container = document.createElement("div");
+document.body.appendChild(container);
+
+const inc = document.createElement('button');
+inc.setAttribute("id", "inc");
+t= document.createTextNode("+");
+container.appendChild(inc);
+inc.appendChild(t);
+
+const dec = document.createElement('button');
+inc.setAttribute("id", "dec");
+t= document.createTextNode("-");
+container.appendChild(dec);
+dec.appendChild(t);
+
+const reset = document.createElement('button');
+inc.setAttribute("id", "reset");
+t= document.createTextNode("Reset");
+container.appendChild(reset);
+reset.appendChild(t);
+
 
 function updateDisplay(a) {
     document.getElementById("counter-label").innerHTML = a;
@@ -16,7 +41,7 @@ dec.addEventListener('click', function(){
     if(counter>0){
     updateDisplay(--counter);
     } else {
-        return;
+      return;
     }
 });
 
